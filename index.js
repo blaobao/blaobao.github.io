33152b01efeb2140f186375e930c84d4b44022ad
blaobao <benduu8@gmail.com>
2026-06-04 00:59:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function initialize(){
 
     let keyboard = [
         ["1", "2", "3", "4", "5", "⌫"],
-        ["✔", "6", "7", "8", "9", "0"]
+        ["6", "7", "8", "9", "0", "✔"]
     ]
 
     for(let i = 0; i<keyboard.length; i++){
